dca9a94f90597fb52711542ac908bd3433005d8e
freewind <nowindlee@gmail.com>
2019-01-06 21:53:08 +0800

    demo

--- hello.ts
+++ hello.ts
@@ -1,1 +1,13 @@
-console.log('Hello');
+import * as minimist from 'minimist';
+import {ParsedArgs} from 'minimist';
+
+type Args = { username: string, password: string }
+
+const argv = process.argv.slice(2);
+
+const cliArgs = minimist<ParsedArgs & Args>(argv, {
+    string: ['username', 'password'],
+    unknown: () => false,
+});
+
+console.log(`Hello, you name is ${cliArgs.username}, password is ${cliArgs.password}`);
